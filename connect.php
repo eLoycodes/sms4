4d4455041a3522124a8758e03bbd483f3a964483
eLoycodes <nirobarlam092000@gmail.com>
2024-09-21 00:59:33 +0800

    n

---
+++ connect.php
@@ -0,0 +1,11 @@
+<?php
+
+$connect = mysqli_connect ("localhost: 3307", "root" , "" , "sms4");
+if(mysqli_connect_errno()){
+    echo "Failed to connect to mysql:", mysqli_connect_error();
+}
+
+?>
+
+
+
--- login.php
+++ login.php
@@ -2,15 +2,15 @@
 
 include("connect.php");
 session_start();
-$email = $password = "";
-$email_error = $password_error = "";
+$username = $password = "";
+$username_error = $password_error = "";
 
 if (isset($_POST['submit'])){
-    if(empty($_POST["email"])){
-		$email_error=" Email is Required!";
+    if(empty($_POST["username"])){
+		$username_error=" Username is Required!";
 	}
     else{
-		$email=$_POST["email"];
+		$username=$_POST["username"];
 	}	
 
   if(empty($_POST["password"])){
@@ -22,19 +22,18 @@ if (isset($_POST['submit'])){
 }
 
 if(isset($_POST["submit"])){
-					$sql="select * from admin where email='{$_POST["email"]}' and password='{$_POST["password"]}'";
+					$sql="select * from admin where username='{$_POST["username"]}' and password='{$_POST["password"]}'";
 					$res=$connect->query($sql);
 					if($res->num_rows>0){
 						$ro=$res->fetch_assoc();
 						$_SESSION["id"]=$ro["id"];
-                        $_SESSION["firstname"]=$ro["firstname"];
-                        $_SESSION["email"]=$ro["email"];
+                        $_SESSION["username"]=$ro["username"];
                         $_SESSION["password"]=$ro["password"];
                       
                         echo "<script>
                             alert('Successfully Login');
                          </script>";
-						echo "<script>window.open('dash.php','_self');</script>";
+						echo "<script>window.open('adminDashboard.php','_self');</script>";
 					}
 					
 				}
@@ -76,14 +75,15 @@ if(isset($_POST["submit"])){
                 <div class="input-group">
                     <label for="username">Username</label>
                     <input type="text" id="username" name="username">
-                    <span class='error' > <?php echo $email_error; ?></span>
+                    <span class='error' > <?php echo $username_error; ?></span>
                 </div>
                 <div class="input-group">
                     <label for="password">Password</label>
-                    <input type="password" id="password" name="password" required>
+                    <input type="password" id="password" name="password">
+                    <span class='error' > <?php echo $password_error; ?></span>
                 </div>
                 <div class="input-group">
-                    <a href="dashboard" class="submit-button">Sign in</a>
+                <button type="submit" class="submit-button" name ="submit">Sign in</button>
 
                 </div>
 
