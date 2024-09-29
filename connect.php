c708f3e9656c16c828a85b94f260fb962e6f3103
eLoycodes <nirobarlam092000@gmail.com>
2024-09-29 17:46:19 +0800

    database first try

--- connect.php
+++ connect.php
@@ -1,6 +1,6 @@
 <?php
 
-$connect = mysqli_connect ("localhost: 3307", "root" , "jbn+^wimGoxE7E!%" , "sms4");
+$connect = mysqli_connect ("localhost", "root" , "jbn+^wimGoxE7E!%" , "sms4");
 if(mysqli_connect_errno()){
     echo "Failed to connect to mysql:", mysqli_connect_error();
 }
