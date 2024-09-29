1df65b33af39aee1991c84e90ac5ab13ca38444e
eLoycodes <nirobarlam092000@gmail.com>
2024-09-29 17:47:10 +0800

    database 2nd try

--- connect.php
+++ connect.php
@@ -1,6 +1,6 @@
 <?php
 
-$connect = mysqli_connect ("localhost", "root" , "jbn+^wimGoxE7E!%" , "sms4");
+$connect = mysqli_connect ("localhost", "regi_adminregistrar" , "jbn+^wimGoxE7E!%" , "regi_bcpregistrar4");
 if(mysqli_connect_errno()){
     echo "Failed to connect to mysql:", mysqli_connect_error();
 }
