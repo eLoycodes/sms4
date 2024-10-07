7e1d6f2d99617f6bf02e5b148fc5ccf845763042
eLoycodes <nirobarlam092000@gmail.com>
2024-10-08 02:42:21 +0800

    a

--- adminDashboard.php
+++ adminDashboard.php
@@ -19,6 +19,11 @@ if(!isset($_SESSION["id"])){
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.1/css/all.min.css">
     <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.5.1/jquery.min.js" charset="utf-8"></script>
 </head>
+<style>
+    a{
+        text-decoration: none;
+    }
+</style>
 <body>
     <div id="sidenav" class="sidenav">
         <img src="image/bcplogo-mini.png" alt="img" class="bcplogo">
