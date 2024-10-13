60d92733d88c797352aa3c476e8c1b579d5fefed
eLoycodes <nirobarlam092000@gmail.com>
2024-10-14 05:49:48 +0800

    last

--- admin-StudentPersonalInformation.php
+++ admin-StudentPersonalInformation.php
@@ -1,8 +1,6 @@
 <?php
 include("connect.php");
-ini_set('session.cookie_httponly', 1);
-ini_set('session.cookie_secure', 1);
-ini_set('session.use_only_cookies', 1);
+
 session_start();
 session_regenerate_id(true);
 
--- adminDashboard.php
+++ adminDashboard.php
@@ -1,9 +1,7 @@
 <?php
 
 include("connect.php");
-ini_set('session.cookie_httponly', 1);
-ini_set('session.cookie_secure', 1); 
-ini_set('session.use_only_cookies', 1);
+
 session_start();
 if(!isset($_SESSION["id"])){
 		echo"<script>window.open('index.php?mes=Access Denied..','_self');</script>";
