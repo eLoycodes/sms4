2eec81a17c895d7fa5677404af36bbe3f5ddcb54
eLoycodes <nirobarlam092000@gmail.com>
2024-10-14 05:44:58 +0800

    got error

--- logout.php
+++ logout.php
@@ -4,5 +4,5 @@ include("connect.php");
 session_start();
 session_unset(); // Remove all session variables
 session_destroy(); // Destroy the session
-header("Location: index.php"); // Redirect to login page
+header("Location: adminDashboard.php"); // Redirect to login page
 exit();
