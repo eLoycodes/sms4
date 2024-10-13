b069f649a2e3ae5707a4c2fb40ea94fa1e56641a
eLoycodes <nirobarlam092000@gmail.com>
2024-10-14 05:33:36 +0800

    aa

--- protected_page.php
+++ protected_page.php
@@ -2,7 +2,7 @@
 session_start();
 
 if (!isset($_SESSION["id"])) {
-    header("Location: index.php?mes=Access Denied..");
+    header("Location: adminDashboard.php?mes=Access Denied..");
     exit();
 }
 
