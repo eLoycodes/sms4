b0b8a2e040688864a81d3213d93cb13ea36e80a4
eLoycodes <nirobarlam092000@gmail.com>
2024-10-14 16:30:51 +0800

    fixing

--- admin-DeactivateList.php
+++ admin-DeactivateList.php
@@ -64,16 +64,19 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST' && isset($_POST['studentID'])) {
         );
 
         if ($insertStmt->execute()) {
-            // Delete from deactivate table
+            // Delete from deactivate table after successful insertion
             $deleteStmt = $connect->prepare("DELETE FROM deactivate WHERE studentID = ?");
             $deleteStmt->bind_param("s", $studentID);
             $deleteStmt->execute();
             $deleteStmt->close();
+            $success_message = "Student deactivated successfully.";
+        } else {
+            $error_message = "Failed to deactivate student: " . $insertStmt->error;
         }
 
         $insertStmt->close();
     } else {
-        echo "Student not found.";
+        $error_message = "Student not found.";
     }
 
     $stmt->close();
@@ -205,47 +208,13 @@ $connect->close();
             </form>
         </div>
     </div>
-</div>
 
-<script type="text/javascript">
-// SideNav
-function toggleNav() {
-    const sidenav = document.getElementById("sidenav");
-    const uppernav = document.getElementById("uppernav");
-
-    if (sidenav.style.left === "0px") {
-        sidenav.style.left = "-280px";
-        uppernav.style.marginLeft = "0";
-    } else {
-        sidenav.style.left = "0";
-        uppernav.style.marginLeft = "280px";
-    }
-}
-
-// Dropdown
-var dropdown = document.getElementsByClassName("dropdown-btn");
-for (var i = 0; i < dropdown.length; i++) {
-    dropdown[i].addEventListener("click", function() {
-        this.classList.toggle("active");
-        var dropdownContent = this.nextElementSibling;
-        dropdownContent.style.display = dropdownContent.style.display === "block" ? "none" : "block";
-    });
-}
-
-var modal = document.getElementById('id01');
-
-window.onclick = function(event) {
-    if (event.target == modal) {
-        modal.style.display = "none";
-    }
-}
-
-// Deactivate
-function openDeactivateModal(studentID, firstName, lastName) {
-    document.getElementById('deactivateStudentID').value = studentID;
-    document.getElementById('deactivateName').innerText = firstName + " " + lastName;
-    document.getElementById('id01').style.display = 'block';
-}
-</script>
+    <script type="text/javascript">
+        function openDeactivateModal(studentID, firstName, lastName) {
+            document.getElementById('deactivateStudentID').value = studentID;
+            document.getElementById('deactivateName').innerText = firstName + " " + lastName;
+            document.getElementById('id01').style.display = 'block';
+        }
+    </script>
 </body>
 </html>
