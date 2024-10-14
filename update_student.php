a48a02c9cf5172954a30695fe41a76dd3665a0ae
eLoycodes <nirobarlam092000@gmail.com>
2024-10-14 14:42:40 +0800

    debug

--- update_student.php
+++ update_student.php
@@ -1,6 +1,13 @@
 <?php
 include("connect.php");
+ini_set('session.cookie_httponly', 1);
+ini_set('session.cookie_secure', 1);
+ini_set('session.use_only_cookies', 1);
 session_start();
+
+// Enable error reporting
+error_reporting(E_ALL);
+ini_set('display_errors', 1);
 
 if (!isset($_SESSION["id"])) {
     echo "<script>window.open('index.php?mes=Access Denied..','_self');</script>";
