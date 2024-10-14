2d4327a28cfa9519a739e8714e95ee6e4de7b81f
eLoycodes <nirobarlam092000@gmail.com>
2024-10-14 14:07:36 +0800

    d

--- adminDashboard.php
+++ adminDashboard.php
@@ -1,6 +1,14 @@
 <?php
 
 include("connect.php");
+ini_set('session.cookie_httponly', 1);
+ini_set('session.cookie_secure', 1);
+ini_set('session.use_only_cookies', 1);
+session_start();
+
+// Enable error reporting
+error_reporting(E_ALL);
+ini_set('display_errors', 1);
 
 session_start();
 if(!isset($_SESSION["id"])){
